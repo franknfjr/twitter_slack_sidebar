c12e12cdbc060129cb6c9ffffd5592c59f1699f8
franknfjr <frankdonascimentojr@gmail.com>
2019-04-21 13:24:47 -0300

    Initial Commit

---
+++ TwitterSlackSidebar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterSlackSidebar
+//
+//  Created by Frank Ferreira on 21/04/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
