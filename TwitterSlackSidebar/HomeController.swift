e8c8346eb23226618f58c36dbdf46d82f0db2cef
franknfjr <frankdonascimentojr@gmail.com>
2019-04-23 10:20:30 -0300

    gerenciando o estado da animação

--- TwitterSlackSidebar/HomeController.swift
+++ TwitterSlackSidebar/HomeController.swift
@@ -19,13 +19,36 @@ class HomeController: UITableViewController {
         setupNavigationItems()
         
         setupHomeController()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
     }
     
     fileprivate let menuWidth: CGFloat = 300
     
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+
+        if gesture.state == .changed {
+            var x = translation.x
+            x = min(menuWidth, x)
+            x = max(0, x)
+            
+            let transform = CGAffineTransform(translationX: x, y: 0)
+            
+            menuController.view.transform = transform
+            navigationController?.view.transform = transform
+        } else if gesture.state == .ended {
+            handleOpen()
+        }
+
+    }
+    
     fileprivate func performAnimation(transform: CGAffineTransform) {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity:  1 ,options: .curveEaseOut, animations: {
             self.menuController.view.transform = transform
+//            self.view.transform = transform
+            self.navigationController?.view.transform = transform
         })
     }
     
