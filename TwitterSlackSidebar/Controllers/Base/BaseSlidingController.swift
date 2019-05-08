f3baad0c8bda4f5de12dc90da628b7b9c74ddb0d
franknfjr <frankdonascimentojr@gmail.com>
2019-05-07 23:17:44 -0300

    base controller para utilizar o menu lateral

---
+++ TwitterSlackSidebar/Controllers/Base/BaseSlidingController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseSlidingController.swift
+//  TwitterSlackSidebar
+//
+//  Created by Frank Ferreira on 07/05/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class BaseSlidingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
