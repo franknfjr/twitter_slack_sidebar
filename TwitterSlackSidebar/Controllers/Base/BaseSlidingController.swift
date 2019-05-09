5eaace620725a657c4754210bdbf8a8d4bc1261c
franknfjr <frankdonascimentojr@gmail.com>
2019-05-09 09:48:43 -0300

    twitter menu ok

--- TwitterSlackSidebar/Controllers/Base/BaseSlidingController.swift
+++ TwitterSlackSidebar/Controllers/Base/BaseSlidingController.swift
@@ -15,6 +15,7 @@ class DarkContainerView: UIView {}
 class BaseSlidingController: UIViewController {
     
     var redViewLeadingConstraint: NSLayoutConstraint!
+    var redViewTrailingConstraint: NSLayoutConstraint!
     fileprivate let menuWidth: CGFloat = 300
     fileprivate let velocityThreshold: CGFloat = 500
     fileprivate var isMenuOpened = false
@@ -55,6 +56,13 @@ class BaseSlidingController: UIViewController {
         
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         view.addGestureRecognizer(panGesture)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss))
+        darkCoverView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTapDismiss(gesture: UIPanGestureRecognizer) {
+        closeMenu()
     }
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
@@ -66,6 +74,7 @@ class BaseSlidingController: UIViewController {
         x = max(0, x)
         
         redViewLeadingConstraint.constant = x
+        redViewTrailingConstraint.constant = x
         darkCoverView.alpha = x / menuWidth
         
         if gesture.state == .ended {
@@ -103,11 +112,13 @@ class BaseSlidingController: UIViewController {
     func openMenu() {
         isMenuOpened = true
         redViewLeadingConstraint.constant = menuWidth
+        redViewTrailingConstraint.constant = menuWidth
         performAnimations()
     }
     
     func closeMenu() {
         redViewLeadingConstraint.constant = 0
+        redViewTrailingConstraint.constant = 0
         isMenuOpened = false
         performAnimations()
     }
@@ -116,7 +127,7 @@ class BaseSlidingController: UIViewController {
         
         performRightViewCleanUp()
         closeMenu()
-
+        
         switch indexPath.row {
         case 0:
             rightViewController = UINavigationController(rootViewController: HomeController())
@@ -162,17 +173,20 @@ class BaseSlidingController: UIViewController {
         NSLayoutConstraint.activate([
             redView.topAnchor.constraint(equalTo: view.topAnchor),
             redView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            redView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
             blueView.topAnchor.constraint(equalTo: view.topAnchor),
-            blueView.trailingAnchor.constraint(equalTo: redView.safeAreaLayoutGuide.leadingAnchor),
+            blueView.trailingAnchor.constraint(equalTo: redView.leadingAnchor),
             blueView.widthAnchor.constraint(equalToConstant: menuWidth),
             blueView.bottomAnchor.constraint(equalTo: redView.bottomAnchor)
             ])
         
-        self.redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
+        redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
         redViewLeadingConstraint.isActive = true
         
+        
+        redViewTrailingConstraint = redView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        redViewTrailingConstraint.isActive = true
+
         setupViewControllers()
     }
     
